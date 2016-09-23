3bd75811fa90614ec937bcb4a008e235278a9470
MishUshakov <mish.ushakov@gmail.com>
2016-09-23 19:26:25 +0200

    Updated, Minified

--- js/website/main.js
+++ js/website/main.js
@@ -1,14 +1,14 @@
-$(window).scroll(function() {
-        if($(window).scrollTop() > 200) {
-            $("#mu").css("background", "linear-gradient(to bottom, rgba(0,0,0,0) 0%, transparent 100%)");
-            $("#mu").css("background-color", "#000312");
-        } else {
-           $("#mu").css("background-color", "transparent");
-           $("#mu").css("background", " linear-gradient(to bottom, rgba(0,0,0,0.65) 0%, transparent 100%)");
-        }
-    });
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 200) {
+        $("#mu").css("background", "linear-gradient(to bottom, rgba(0,0,0,0) 0%, transparent 100%)");
+        $("#mu").css("background-color", "#000312");
+    } else {
+        $("#mu").css("background-color", "transparent");
+        $("#mu").css("background", " linear-gradient(to bottom, rgba(0,0,0,0.65) 0%, transparent 100%)");
+    }
+});
 
-$('a').click(function(){
+$('a').click(function () {
     $('html, body').animate({
         scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
     }, 500);
