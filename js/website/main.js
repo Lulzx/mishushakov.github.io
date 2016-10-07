da56800d85d7907e2c9d4a54a2233340424d8dd1
MishUshakov <mish.ushakov@gmail.com>
2016-10-07 17:15:12 +0200

    Updated files and optimized javascript imports

--- js/website/main.js
+++ js/website/main.js
@@ -7,10 +7,3 @@ $(window).scroll(function () {
         $("#mu").css("background", " linear-gradient(to bottom, rgba(0,0,0,0.65) 0%, transparent 100%)");
     }
 });
-
-$('a').click(function () {
-    $('html, body').animate({
-        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-    }, 500);
-    return false;
-});
